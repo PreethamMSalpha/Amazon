cb2cb8761a6687a393832185a20872c329c5225c
Preetham M.S <preethamms619@gmail.com>
2021-06-15 14:08:35 +0530

    add custom middlewares, userRoutes and userControllers

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+//my routes
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //DB connection
 mongoose
@@ -28,6 +30,7 @@ app.use(cors());
 
 //routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 3000;
--- controllers/auth.js
+++ controllers/auth.js
@@ -320,3 +320,32 @@ exports.resetPassword = async (req, res, next) => {
     });
   }
 };
+
+//protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  algorithms: ["HS256"],
+  userProperty: "auth",
+});
+
+//custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not ADMIN, Access denied",
+    });
+  }
+  next();
+};
+
+//through frontend we are gonna make property inside the user "profile" and this property is gonna set only if the user is signed in
